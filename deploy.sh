64c41885032c43c1e2ea96a3e405fa73b958e1e6
LucaVmu <lucavmu@outlook.de>
2026-04-07 16:09:22 +0200

    Made a deployment script (finally)

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+echo 'Make sure you ran "npm run build" beforehand.'
+
+ssh "$1" /bin/sh <<'EOF'
+set -e
+rm -r "$HOME/nyxian.app"
+EOF
+
+scp -rC "$(dirname "$(realpath "$0")")/build" "$1:~/nyxian.app"
+
+ssh "$1" /bin/sh <<'EOF'
+set -e
+mv --exchange -T "$HOME/nyxian.app" /var/www/nyxian.app
+rm -rf "$HOME/nyxian.app"
+EOF
+
+echo "Deployed successfully!"
